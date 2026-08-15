3bcbada876bac302d20adcb0040e43fff832abb5
tunisha43 <royalkaribo@gmail.com>
2026-08-15 19:16:04 +0100

    Update client.ts

--- campusmind-nextjs-starter/campusmind-nextjs-starter/lib/supabase/client.ts
+++ campusmind-nextjs-starter/campusmind-nextjs-starter/lib/supabase/client.ts
@@ -1,2 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
-export function createClient(){return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!)}
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  );
+}
